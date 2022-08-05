7357c2b8202f3649bf538b71b787803c816587d1
NikitaP <unntiss@bk.ru>
2022-08-05 21:29:09 +0300

    task1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// семинар 1 
+
+// 1 Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int sqrt = number * number;
+// number *= number;
+int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.WriteLine("Число в квадрате: " + sqrt1);
